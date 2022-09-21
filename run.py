2dac1aa69e6e5559d5d5fe62758f70c86af638c6
胡飞 <https://github.com/>
2022-09-21 17:38:23 +0800

    UI

--- run.py
+++ run.py
@@ -6,4 +6,5 @@ import pytest
 if __name__ == '__main__':
     pytest.main()#执行所有用例
     time.sleep(3)
+    print('cssss')
     # os.system("allure generate ./temps -o ./Test_Result/Test_Report --clean")
